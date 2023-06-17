03fcc2e918eb267122643874c8da20b06aa5ae58
efrizalnf <efrizalnf@gmail.com>
2023-06-17 21:16:38 +0700

    setup counter state

--- lib/bloc/counter_state.dart
+++ lib/bloc/counter_state.dart
@@ -1,23 +1,42 @@
 part of 'counter_bloc.dart';
 
+enum CounterStatus { initial, loading, success, error }
+
 class CounterState extends Equatable {
-  const CounterState({required this.counter});
-  factory CounterState.initial() => const CounterState(counter: 0);
+  const CounterState({
+    required this.counter,
+    required this.counterStatus,
+    required this.error,
+  });
   final int counter;
+  final CounterStatus counterStatus;
+  final String error;
+
+  factory CounterState.initial() => const CounterState(
+        counter: 0,
+        counterStatus: CounterStatus.initial,
+        error: '',
+      );
 
   @override
-  List<Object> get props => [counter];
+  List<Object> get props => [counter, counterStatus, error];
+
+  @override
+  bool get stringify => true;
 
   CounterState copyWith({
     int? counter,
   }) {
     return CounterState(
       counter: counter ?? this.counter,
+      counterStatus: counterStatus ?? this.counterStatus,
+      error: error ?? this.error,
     );
   }
 
   @override
-  String toString() => 'CounterState(counter: $counter, state : C, error : )';
+  String toString() =>
+      'CounterState(counter: $counter, counterState : $counterStatus, error : $error)';
 }
 
 // class CounterInitial extends CounterState {
